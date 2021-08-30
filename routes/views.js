5ee5bb7adb96b54eac1467331d380e7f7f86299f
lara <lara@vehicle4change.org>
2021-08-30 18:00:27 -0400

    file structure added

---
+++ routes/views.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/exercise", (req, res) => {
+});
+
+router.get("/stats", (req, res) => {
+});
+
+module.exports = router;
