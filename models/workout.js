21d57371e6d96a9bb14a02b470bbecf79500ea77
lara <lara@vehicle4change.org>
2021-09-01 18:54:04 -0400

    added models

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-//define fields
-
+//day: numeric
+//new Date() returns the current date as a Date object.  
+day: {
+    type: Date,
+    default: () => new Date(),
+    },
+/*  seeds fields (array of objects)
+    type: 'resistance',
+    name: 'Push Press',
+    duration: 25,
+    weight: 185,
+    reps: 8,
+    sets: 4, 
+    distance*/    
+exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Select the exercise type',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter a name for the exercise',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter the duration in minutes',
+      },//conditional or optional
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
