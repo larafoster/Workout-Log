56f9bcfbf5d9d2bba7c2004e8055909256a84c3b
lara <lara@vehicle4change.org>
2021-09-05 13:47:59 -0400

    server changes to connect to mongo atlas

--- server.js
+++ server.js
@@ -12,13 +12,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
   //seed file
-});
-
+}); */
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout-app-osu',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/views.js"));
